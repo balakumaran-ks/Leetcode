e4cedd70db33a8e562f6e60c10fdf4f1e19694fa
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2025-11-25 19:40:11 +0530

    Time: 55 ms (8.44%) | Memory: 55.4 MB (5.28%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -3,10 +3,9 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    let count = n-1;
+    
     return function() {
-        count = count+1;
-        return count;
+        return n++;
     };
 };
 
