026eb36e5341a5c155d13f509198b4a4da37cd4d
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-02-07 10:41:15 +0530

    Time: 9 ms (98.55%) | Memory: 59.4 MB (70.51%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,38 +10,36 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null) return head;
+        if(head==null || head.next==null)return head;
 
-        ListNode slow = head , fast = head.next;
-        while(fast!=null && fast.next!= null){
-            slow = slow.next;
+
+        ListNode slow=head,fast=head.next;
+        while(fast!=null && fast.next!=null){
             fast = fast.next.next;
+            slow = slow.next;
         }
-
         ListNode mid = slow.next;
         slow.next = null;
-
         ListNode left = sortList(head);
         ListNode right = sortList(mid);
 
         return merge(left,right);
     }
 
-    ListNode merge(ListNode l1,ListNode l2){
+    public static ListNode merge(ListNode l1,ListNode l2){
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-
         while(l1!=null && l2!=null){
             if(l1.val<l2.val){
                 tail.next = l1;
-                l1=l1.next;
+                l1 = l1.next;
             }else{
                 tail.next = l2;
-                l2 = l2.next;
+                l2=l2.next;
             }
             tail = tail.next;
         }
-        tail.next = (l1!=null) ? l1:l2;
+        tail.next = (l1==null)?l2:l1;
         return dummy.next;
     }
 }
