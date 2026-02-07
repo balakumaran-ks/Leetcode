81c14c05d99ff35c8cbffc2e78d1b0c28cb1dc3d
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-02-07 10:32:41 +0530

    Time: 5 ms (86.44%) | Memory: 46.8 MB (8.59%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -10,26 +10,19 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ListNode sorted =null;
-        ListNode curr = head;
-        while(curr!=null){
-            ListNode temp = curr.next;
-            sorted = sortedIndex(sorted,curr);
-            curr = temp;
+        ListNode temp = head;
+        int i=0;
+        List<Integer> list = new ArrayList<>();
+        while(temp!=null){
+            list.add(temp.val);
+            temp=temp.next;
         }
-        return sorted;
-    }
-
-    public static ListNode sortedIndex(ListNode sorted,ListNode newNode){
-        if(sorted==null || newNode.val<sorted.val){
-            newNode.next = sorted;
-            return newNode;
-        }else{
-            ListNode curr = sorted;
-            while(curr.next!=null && curr.next.val<newNode.val)curr=curr.next;
-            newNode.next = curr.next;
-            curr.next = newNode;
-            return sorted;
+        temp=head;
+        Collections.sort(list);
+        while(temp!=null){
+            temp.val = list.get(i++);
+            temp = temp.next;
         }
+        return head;
     }
 }
