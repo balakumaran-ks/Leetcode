ad67d7e68a272a6f72bfe72aa05a6bafa8b44d03
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-02-07 10:49:29 +0530

    Time: 21 ms (86.56%) | Memory: 119 MB (93.51%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -1,14 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        java.util.HashSet<Integer> mpp = new java.util.HashSet<>();
-        for (int x : nums) mpp.add(x);
-        while (head != null && mpp.contains(head.val)) head = head.next;
-        ListNode curr = head;
-        while (curr != null && curr.next != null) {
-            while (curr.next != null && mpp.contains(curr.next.val)) {
-                curr.next = curr.next.next;
-            }
-            curr = curr.next;
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums)set.add(num);
+        while(head!=null&&set.contains(head.val))head=head.next;
+        ListNode curr=head;
+        while(curr!=null){
+            while(curr.next!=null && set.contains(curr.next.val))curr.next=curr.next.next;
+            curr=curr.next;
         }
         return head;
     }
