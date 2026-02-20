bf9339245d409fd6bcd485d9e7f3308ac8e6ca25
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-02-20 11:47:46 +0530

    Time: 4 ms (42.15%) | Memory: 43.3 MB (58.03%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length()%2!=0)return false;
         Stack<Character> stack = new Stack<>();
         Map<Character, Character> mapping = new HashMap<>();
         mapping.put(')', '(');
