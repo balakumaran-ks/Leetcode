d4efa20698919d8a3bcdd670d3894bf9ea03cbd2
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-04-18 09:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (53.18%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -1,13 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) return false;
 
-        if (root.left == null && root.right == null) {
-            return targetSum - root.val == 0;
+    public boolean helper(TreeNode node,int target,int sum){
+        boolean l=false,r=false;
+        if(node.right==null && node.left==null){
+            if(target==(node.val+sum))return true;
+            return false;
         }
+        if(node.left!=null){
+            l =  helper(node.left,target,sum+node.val);
+        }
+        if(node.right!=null){
+            r =  helper(node.right,target,sum+node.val);
+        }
+        return l||r;
+    }
 
-        targetSum -= root.val;
-
-        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);        
+    public boolean hasPathSum(TreeNode node, int targetSum) {
+        if(node==null)return false;
+        return helper(node,targetSum,0);
     }
 }
