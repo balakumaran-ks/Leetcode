78b4c0790e8557633d2d11749dd73adc77023d19
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-04-18 13:19:29 +0530

    Time: 8 ms (73.09%) | Memory: 105.6 MB (27.20%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        int[] digits = new int[10];
+		return psudoHelper(root, digits);
+    }
+    private int psudoHelper(TreeNode node, int[] digits) {
+		if (node == null)
+			return 0;
+		// condition check
+		if (node.left == null && node.right == null) {
+			int digitInd = node.val;
+
+			// do
+			digits[digitInd] = digits[digitInd] ^ 1;
+			int res = 0;
+
+			// calculate
+			for (int digit : digits)
+				res += digit;
+
+			// backtrack
+			digits[digitInd] = digits[digitInd] ^ 1;
+			if (res <= 1)
+				return 1;
+			return 0;
+		}
+		int leftSum = 0;
+		int rightSum = 0;
+		if (node.left != null) {
+			int digitInd = node.val;
+			digits[digitInd] = digits[digitInd] ^ 1;
+			leftSum = psudoHelper(node.left, digits);
+			digits[digitInd] = digits[digitInd] ^ 1;
+		}
+		if (node.right != null) {
+			int digitInd = node.val;
+			digits[digitInd] = digits[digitInd] ^ 1;
+			rightSum = psudoHelper(node.right, digits);
+			digits[digitInd] = digits[digitInd] ^ 1;
+		}
+		return leftSum + rightSum;
+
+	}
+
+}
