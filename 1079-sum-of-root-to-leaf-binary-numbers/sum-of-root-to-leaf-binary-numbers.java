ee2e191dda6c422f262e6d661b780c60cde55ef0
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-04-19 20:48:29 +0530

    Time: 2 ms (15.64%) | Memory: 43.8 MB (66.08%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -1,20 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    
-    public int sumRootToLeaf(TreeNode root) {
-        return dfs(root, 0);
+    int sum = 0;
+
+    void helper(TreeNode node,StringBuilder s){
+        //leaf node
+        if(node.left==null && node.right==null){
+            s.append(node.val);
+            //convert to decimal
+            int num = Integer.parseInt(s.toString(),2);
+            this.sum +=num;
+            s.deleteCharAt(s.length()-1);
+            return;
+        }
+        s.append(node.val);
+        if(node.left!=null){
+            helper(node.left,s);
+        }
+        if(node.right!=null){
+            helper(node.right,s);
+        }
+        s.deleteCharAt(s.length()-1);
+        return;
     }
-    
-    private int dfs(TreeNode node, int current) {
-        if (node == null) {
-            return 0;
-        }
-        
-        current = current * 2 + node.val;
-        
-        if (node.left == null && node.right == null) {
-            return current;
-        }
-        
-        return dfs(node.left, current) + dfs(node.right, current);
+
+    public int sumRootToLeaf(TreeNode root) {
+        StringBuilder s = new StringBuilder(); //to store the path
+        helper(root,s);
+        return this.sum;
     }
 }
