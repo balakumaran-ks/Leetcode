8765c97ffd6a8811d4e83f8cd45f454f7544ff63
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-04-20 22:34:51 +0530

    Time: 4 ms (1.01%) | Memory: 43.6 MB (38.07%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,9 +1,32 @@
 class Solution {
+    List<List<Integer>> mem;
+    Solution(){
+        mem = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                // If it's the first or last element of the row, it's always 1
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    // Standard DP: current = (prevRow, j-1) + (prevRow, j)
+                    int left = mem.get(i - 1).get(j - 1);
+                    int right = mem.get(i - 1).get(j);
+                    row.add(left + right);
+                }
+            }
+            mem.add(row);
+        }
+    }
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
         
         for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
+            if(i<this.mem.size()){
+                res.add(mem.get(i));
+            }else{
+                List<Integer> row = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
                 // If it's the first or last element of the row, it's always 1
                 if (j == 0 || j == i) {
@@ -16,6 +39,7 @@ class Solution {
                 }
             }
             res.add(row);
+            }
         }
         return res;
     }
