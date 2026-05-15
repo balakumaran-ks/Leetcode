9f63b8a2c1f6202da4cbf87caab39983d602bf4b
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-05-15 08:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (36.12%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -2,6 +2,7 @@ class Solution {
     public int findMin(int[] nums) {
         //base case
         if(nums.length==1)return nums[0];
+        
         // 2 pointer + point of dip
         for(int l=0,r=nums.length-1;l<=r;l++,r--){
             //finding point of dip
