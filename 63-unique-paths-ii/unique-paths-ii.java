eb29fabb5583c904f717b0ac591128f452a9f913
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-06-16 21:14:25 +0530

    Time: 1 ms (17.28%) | Memory: 43.6 MB (44.95%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,53 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int[][] path = new int[row][col];
-        for(int i=0;i<col;i++){
-            if(grid[0][i]!=1)path[0][i]=1;
-            else break;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if(m==1&&n==1){
+            if(grid[0][0]==0)return 1;
+            else return 0;
         }
-        for(int i=0;i<row;i++){
-            if(grid[i][0]!=1)path[i][0]=1;
-            else break;
+
+        if(grid[0][0]==1)return 0;
+
+        grid[0][0] = 1;
+        
+        //first row filling
+        for(int col=1;col<n;col++){
+            if(grid[0][col]==1){
+                grid[0][col]=-1;
+                continue;
+            }
+            grid[0][col] = grid[0][col-1];
         }
-        for(int i=1;i<row;i++){
-            for(int j=1;j<col;j++){
-                if(grid[i][j]!=1){
-                    path[i][j]=path[i-1][j]+path[i][j-1];
-                }else
-                path[i][j]=0;
+
+        //first col filling
+        for(int row=1;row<m;row++){
+            if(grid[row][0]==1){
+                grid[row][0]=-1;
+                continue;
+            }
+            grid[row][0] = grid[row-1][0];
+        }
+
+        //matrix filling
+        for(int row=1;row<m;row++){
+            for(int col=1;col<n;col++){
+                if(grid[row][col]==1){
+                    grid[row][col]=-1;
+                    continue;
+                }
+                if(grid[row][col-1]==-1 && grid[row-1][col]==-1)continue;
+                else if(grid[row][col-1]!=-1 && grid[row-1][col]!=-1){
+                    grid[row][col] = grid[row][col-1] + grid[row-1][col];
+                }
+                else{
+                    grid[row][col] = Math.max(grid[row][col-1] , grid[row-1][col]);
+                }
             }
         }
-        return path[row-1][col-1];
+
+        grid[m-1][n-1] = grid[m-1][n-1]==-1 ? 0 : grid[m-1][n-1];
+        return grid[m-1][n-1];
     }
 }
