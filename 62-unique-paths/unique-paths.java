7a31e095c4b7e16a348affd6f08057758796f815
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-06-16 20:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (64.97%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,13 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       int[][] path = new int[m][n];
-       for(int ind=0;ind<n;ind++)path[0][ind]=1;
-       for(int ind=0;ind<m;ind++)path[ind][0]=1;
-       for(int row=1;row<m;row++){
-        for(int col=1;col<n;col++){
-            path[row][col] = path[row-1][col]+path[row][col-1];
+        if(n==1 || m==1)return 1;
+
+        //building 2D matrix
+        int[][] matrix = new int[m][n];
+
+        //first row => 1
+        for(int col=1;col<n;col++)matrix[0][col] = 1;
+
+        //first col => 1
+        for(int row=1;row<m;row++)matrix[row][0] = 1;
+
+        //matrix filling
+        for(int row = 1;row<m;row++){
+            for(int col=1;col<n;col++){
+                matrix[row][col] = matrix[row-1][col] + matrix[row][col-1];
+            }
         }
-       } 
-       return path[m-1][n-1];
+
+        return matrix[m-1][n-1];
     }
 }
