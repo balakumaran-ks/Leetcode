a7e2888a35eeb5c3de0399b652de70641b2a387d
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-06-23 11:31:40 +0530

    Time: 9 ms (10.47%) | Memory: 46.8 MB (28.27%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
@@ -1,51 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode recoverFromPreorder(String traversal) {
         Stack<TreeNode> stack = new Stack<>();
-        int i = 0;
+        int i=0;
         int n = traversal.length();
-        
-        while (i < n) {
-            // 1. Count the depth (number of dashes)
-            int depth = 0;
-            while (i < n && traversal.charAt(i) == '-') {
+
+        while(i<n){
+
+            int depth=0;
+            while(i<n && traversal.charAt(i)=='-' ){
+                i++;
                 depth++;
-                i++;
             }
-            
-            // 2. Extract the node value
+
             int val = 0;
-            while (i < n && Character.isDigit(traversal.charAt(i))) {
-                val = val * 10 + (traversal.charAt(i) - '0');
+
+            while(i<n && Character.isDigit(traversal.charAt(i))){
+                val = val*10 + (traversal.charAt(i)-'0');
                 i++;
             }
-            
-            // Create the current node
-            TreeNode node = new TreeNode(val);
-            
-            // 3. Move up the tree if the stack is too deep
-            while (stack.size() > depth) {
+
+            TreeNode curr = new TreeNode(val);
+            while(stack.size()>depth){
                 stack.pop();
             }
-            
-            // 4. Connect to the parent
-            if (!stack.isEmpty()) {
-                TreeNode parent = stack.peek();
-                if (parent.left == null) {
-                    parent.left = node;
-                } else {
-                    parent.right = node;
-                }
-            }
-            
-            // 5. Push current node to stack for its potential children
-            stack.push(node);
+            if(stack.size()>0){
+            if(stack.peek().left==null)stack.peek().left=curr;
+            else stack.peek().right=curr;}
+            stack.push(curr);
         }
-        
-        // The bottom-most element in the stack is the root
-        while (stack.size() > 1) {
-            stack.pop();
-        }
-        
+        while(stack.size()>1)stack.pop();
         return stack.peek();
     }
 }
