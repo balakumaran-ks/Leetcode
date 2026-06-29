cbf58c8ae9d28fcc1c184f93f37ce16c77b0ac3a
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-06-29 10:13:06 +0530

    Time: 1 ms (95.28%) | Memory: 49.6 MB (72.96%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,47 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-
-    public void fillMap(TreeNode node, int level , Map<Integer,List<TreeNode>> map ){
-        if(node==null)return;
-        if(level%2==1){
-            map.putIfAbsent(level,new ArrayList<TreeNode>());
-            map.get(level).add(node);
-        }
-        fillMap(node.left,level+1,map);
-        fillMap(node.right,level+1,map);
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) return null;
+        // Start the DFS with the left and right children at level 1 (odd)
+        dfs(root.left, root.right, 1);
+        return root;
     }
 
-    public TreeNode reverseOddLevels(TreeNode root) {
-        Map<Integer,List<TreeNode>> map = new HashMap<>();
-        fillMap(root,0,map);
-        int level = 1;
-        while(map.containsKey(level)){
-            List<TreeNode> list = map.get(level);
-            int l = 0;
-            int r = list.size()-1;
-            while(l<r){
-                int temp = list.get(l).val;
-                list.get(l).val = list.get(r).val;
-                list.get(r).val = temp;
-                l++;
-                r--;
-            }
-            level+=2;
+    private void dfs(TreeNode node1, TreeNode node2, int level) {
+        if (node1 == null || node2 == null) {
+            return;
         }
-        return root;
+
+        // If the current level is odd, swap the values directly
+        if (level % 2 == 1) {
+            int temp = node1.val;
+            node1.val = node2.val;
+            node2.val = temp;
+        }
+
+        // Cruise down symmetrically:
+        // Left child of node1 pairs with Right child of node2
+        dfs(node1.left, node2.right, level + 1);
+        // Right child of node1 pairs with Left child of node2
+        dfs(node1.right, node2.left, level + 1);
     }
 }
