3597a08463e8e557640ac47c2e77ad8f1597edcb
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-07-18 23:51:10 +0530

    Time: 15 ms (65.49%) | Memory: 47.8 MB (33.24%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -7,8 +7,6 @@ class Solution {
         int n = grid.length;
         if(n==1 && grid[0][0]==0)return 1;
 
-        boolean[][] visited = new boolean[n][n];
-        visited[0][0] = true;
         int[][] dist = new int[n][n];
         Queue<int[]> queue = new ArrayDeque<>();
         queue.add(new int[]{0,0});
@@ -19,8 +17,8 @@ class Solution {
             for(int i=0;i<8;i++){
                 int x = r+dr[i];
                 int y = c+dc[i];
-                if(x>=0 && x<n && y>=0 && y<n && grid[x][y]==0 && !visited[x][y]){
-                    visited[x][y] = true;
+                if(x>=0 && x<n && y>=0 && y<n && grid[x][y]==0 && dist[x][y]==0){
+                    // visited[x][y] = true;
                     dist[x][y] = dist[r][c]+1;
                     queue.add(new int[]{x,y});
                 }
