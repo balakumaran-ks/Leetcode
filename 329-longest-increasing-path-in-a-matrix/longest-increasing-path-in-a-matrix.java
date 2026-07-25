aa26993770326b4eddc7c4aaf05f8d1316c3627c
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-07-25 19:16:37 +0530

    Time: 12 ms (18.38%) | Memory: 48 MB (5.15%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -3,16 +3,14 @@ class Solution {
     int[] dx = {0,0,1,-1};
     int[] dy = {1,-1,0,0};
 
-    public int dfs(int[][] adj , int r , int c){
+    public int dfs(int[][] adj , int r , int c,int m,int n){
         if(memo[r][c]!=0)return memo[r][c];
         memo[r][c]=1;
-        // int m = ;
-        // int n = ;
         for(int d=0;d<4;d++){
             int nr = r + dx[d];
             int nc = c + dy[d];
-            if(nr>=0 && nr < adj.length && nc>=0 && nc < adj[0].length && adj[nr][nc]>adj[r][c]){
-                memo[r][c] = Math.max(memo[r][c],1+dfs(adj,nr,nc));
+            if(nr>=0 && nr < m && nc>=0 && nc < n && adj[nr][nc]>adj[r][c]){
+                memo[r][c] = Math.max(memo[r][c],1+dfs(adj,nr,nc,m,n));
             }
         }
         return memo[r][c];
@@ -26,7 +24,7 @@ class Solution {
         // Arrays.fill(mem,-1);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                res = Math.max(res , dfs(matrix,i,j));
+                res = Math.max(res , dfs(matrix,i,j,m,n));
             }
         }    
         return res;
