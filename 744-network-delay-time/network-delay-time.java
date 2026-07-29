342b7992877ee8adf860139faccf0f98445d5ece
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-07-29 12:19:18 +0530

    Time: 126 ms (5.01%) | Memory: 49.6 MB (23.42%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public void dfs(int[][] adj , int u , int[] dist){
+        for(int v=0;v<adj[u].length;v++){
+            if(adj[u][v]!=-1 && dist[v]>dist[u]+adj[u][v]){
+                dist[v] = Math.min(dist[v],dist[u]+adj[u][v]);
+                dfs(adj,v,dist);
+            }
+        }
+    }
+
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[] dist = new int[n+1];
+        Arrays.fill(dist , Integer.MAX_VALUE);
+        dist[k] = 0;
+        int[][] adj = new int[n+1][n+1];
+        for(int[] a:adj)Arrays.fill(a,-1);
+        for(int[] edge:times){
+            adj[edge[0]][edge[1]] = edge[2];
+        }
+        for(int[] a:adj)System.out.println(Arrays.toString(a));
+        dfs(adj , k , dist);
+        System.out.println(Arrays.toString(dist));
+        int res = Integer.MIN_VALUE;
+        for(int d=1;d<=n;d++){
+            if(dist[d]==Integer.MAX_VALUE)return -1;
+            res = Math.max(res,dist[d]);
+        }
+        return res;
+    }
+}
