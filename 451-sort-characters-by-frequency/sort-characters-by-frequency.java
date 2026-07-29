07d52013457e25447347f35055ebaec573e66164
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-07-29 11:40:27 +0530

    Time: 17 ms (26.59%) | Memory: 47.4 MB (19.59%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -9,33 +9,14 @@ class Ele{
 
 class Solution {
     public String frequencySort(String s) {
-        int[] caps = new int[26];
-        int[] small = new int[26];
-        int[] digits  = new int[10];
+        Map<Character,Integer> map = new HashMap<>();
         for(char c:s.toCharArray()){
-            if(c<='z'&&c>='a')small[c-'a']++;
-            else if(c>='A' && c<='Z')caps[c-'A']++;
-            else digits[c-'0']++;
+            map.put(c,map.getOrDefault(c,0)+1);
         }
         List<Ele> list = new ArrayList<>();
-        for(int i=0;i<26;i++){
-            if(small[i]!=0){
-                char c =(char) ('a'+ i);
-                Ele obj = new Ele(c,small[i]);
-                for(int j=0;j<small[i];j++)list.add(obj);
-            }
-            if(caps[i]!=0){
-                char c = (char)(i+'A');
-                Ele obj = new Ele(c,caps[i]);
-                for(int j=0;j<caps[i];j++)list.add(obj);
-            }
-        }
-        for(int i=0;i<10;i++){
-            if(digits[i]!=0){
-                char c = (char)('0'+i);
-                Ele obj = new Ele(c,digits[i]);
-                for(int j=0;j<digits[i];j++)list.add(obj);
-            }
+        for(Map.Entry<Character,Integer> e:map.entrySet()){
+            Ele obj = new Ele(e.getKey(),e.getValue());
+            for(int i=0;i<obj.freq;i++)list.add(obj);
         }
 
         Collections.sort(list , (a,b)->{
