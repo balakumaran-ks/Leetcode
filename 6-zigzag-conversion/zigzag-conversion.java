8325484109b81f462cbb3c781e80ce01f966999b
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-08-05 19:22:31 +0530

    Time: 5 ms (52.56%) | Memory: 46.9 MB (23.15%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -2,31 +2,22 @@ class Solution {
     public String convert(String s, int numRows) {
         int n = s.length();
         if(n<=numRows || numRows==1)return s;
-        char[][] matrix = new char[numRows][n];
+        StringBuilder[] arr = new StringBuilder[numRows];
+        for(int i=0;i<numRows;i++)arr[i]=new StringBuilder();
         int index = 0;
-        int i=0;
-        int j=0;
+        int i = 0;
         while(index<n){
             while(index<n && i!=numRows){
-                matrix[i][j] = s.charAt(index);
-                index++;
-                i++;
+                arr[i++].append(s.charAt(index++));
             }
             i-=2;
             while(index<n && i>=0){
-                matrix[i--][++j]=s.charAt(index);
-                index++;
-                // if(i>=0)j++;
+                arr[i--].append(s.charAt(index++));
             }
             i+=2;
         }
-        // for(char[] arr:matrix)System.out.println(Arrays.toString(arr));
-        StringBuilder sb = new StringBuilder();
-        for(char[] arr:matrix){
-            for(char c:arr){
-                if(c=='.'||c==','||(c>='a'&&c<='z')||(c>='A'&&c<='Z'))sb.append(c);
-            }
-        }
-        return sb.toString();
+        StringBuilder res = new StringBuilder();
+        for(i=0;i<numRows;i++)res.append(arr[i].toString());
+        return res.toString();
     }
 }
