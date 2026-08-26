bba8dbeefca967047ef6368c071d8f8f82407089
Balakumaran K S <137416795+balakumaran-ks@users.noreply.github.com>
2026-08-26 19:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (43.60%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,35 +1,24 @@
 class Solution {
 
-    public void dfs(int n,boolean[] visited,int[][] matrix){
-        visited[n]=true;
-        for(int i=0;i<visited.length;i++){
-            if(matrix[n][i]==1 && visited[i]==false){
-                dfs(i,visited,matrix);
+    public void dfs(int u , int[][] adj , boolean[] visited){
+        for(int i=0;i<adj.length;i++){
+            if(!visited[i] && adj[u][i]==1){
+                visited[i] = true;
+                dfs(i,adj,visited);
             }
         }
-    }
-
-    public boolean allVisit(boolean[] visited){
-        for(boolean b:visited){
-            if(b==false)return b;
-        }
-        return true;
-    }
+    } 
 
     public int findCircleNum(int[][] isConnected) {
-        int cities = isConnected.length;
-        int provinces = 0;
-        boolean[] visited = new boolean[cities];
-        int city = 0;
-        while(allVisit(visited)==false){
-            inner:
-            for(int i=0;i<cities;i++)if(visited[i]==false){
-                city=i;
-                break inner;
-                }
-            dfs(city,visited,isConnected);
-            provinces++;
+        int res = 0;
+        boolean[] visited = new boolean[isConnected.length];
+        for(int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(i,isConnected,visited);
+                res++;
+            }
         }
-        return provinces;
+        return res;
     }
 }
